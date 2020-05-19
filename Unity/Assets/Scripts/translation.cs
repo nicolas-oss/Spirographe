a9237174ccb8aca0f7ee517a711477a45ffcc30f
nicolas-oss <59309078+nicolas-oss@users.noreply.github.com>
2020-05-19 19:49:43 +0200

    Import Base

---
+++ Unity/Assets/Scripts/translation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class translation : MonoBehaviour
+{
+	public float V1 = 7.0f;
+	public float A = 20.0f;
+	public float V2 = 20.0f;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, V2*Mathf.Sin(Time.deltaTime*A), 0);
+    }
+}
