623c3cfcfbc52c786b051d9199794c21de623b02
nicolas-oss <59309078+nicolas-oss@users.noreply.github.com>
2020-05-22 17:22:06 +0200

    interface WIP
    affichage boites options OK

--- Unity/Assets/Scripts/Interface.cs
+++ Unity/Assets/Scripts/Interface.cs
@@ -9,7 +9,10 @@ public class Interface : MonoBehaviour
     public Color UnselectedColor,SelectedColor;
 	
 	public UnityEvent MainEvent;
-	public GameObject ButtonDisque1,ButtonDisque2,ButtonDisque3;
+	public GameObject ButtonEchelle,ButtonRotation,ButtonDisque1,ButtonDisque2,ButtonDisque3,ButtonCrayon;
+	public GameObject PanelOptionButtonEchelle,PanelOptionButtonRotation,PanelOptionButtonDisque1,PanelOptionButtonDisque2,PanelOptionButtonDisque3,PanelOptionButtonCrayonX,PanelOptionButtonCrayonY;
+	public GameObject Surface;
+	GameObject ActiveButton;
 	//public GameObject PreviousSelectedButton;
 	
     void Start()
@@ -19,7 +22,7 @@ public class Interface : MonoBehaviour
 		{
 			MainEvent = new UnityEvent();
 		}
-		MainEvent.AddListener(EventR1);
+		MainEvent.AddListener(CliqueSurfaceDeTravail);
     }
 
     // Update is called once per frame
@@ -34,11 +37,10 @@ public class Interface : MonoBehaviour
 	public void SelectButton(GameObject PressedButton)
 	{
 		DeselectAllButton();
+		HideAllOptionPanels();
 		SetColorActive(PressedButton);
-		if (PressedButton==ButtonDisque1)
-		{
-				Debug.Log("ButtonDisque1");
-		}
+		ActiveButton = PressedButton;
+		ShowCurrentOptionPanel();
 	}
 	
 	public void DeselectAllButton()
@@ -54,12 +56,32 @@ public class Interface : MonoBehaviour
 		}
 	}
 	
+	public void HideAllOptionPanels()
+	{
+		GameObject[] AllOptionPanels;
+		AllOptionPanels = GameObject.FindGameObjectsWithTag("PanelOption");
+		foreach(GameObject InspectedPanel in AllOptionPanels)
+		{
+			InspectedPanel.active=false;
+		}	
+	}
+	
 	public void SetColorActive(GameObject PressedButton)
 	{
 		var MyButton=PressedButton.GetComponent<Button>();
 		var colors = MyButton.colors;
         colors.normalColor = SelectedColor;
         MyButton.colors = colors;
+	}
+	
+	public void ShowCurrentOptionPanel()
+	{
+		if (ActiveButton==ButtonEchelle) {PanelOptionButtonEchelle.active=true;}
+		if (ActiveButton==ButtonRotation) {PanelOptionButtonRotation.active=true;}
+		if (ActiveButton==ButtonDisque1) {PanelOptionButtonDisque1.active=true;}
+		if (ActiveButton==ButtonDisque2) {PanelOptionButtonDisque2.active=true;}
+		if (ActiveButton==ButtonDisque3) {PanelOptionButtonDisque3.active=true;}
+		if (ActiveButton==ButtonCrayon) {PanelOptionButtonCrayonX.active=true; PanelOptionButtonCrayonY.active=true;}
 	}
 	
 	public void EventR1()
@@ -70,5 +92,17 @@ public class Interface : MonoBehaviour
 	public void EventR2()
 	{
 		Debug.Log("EventR2");
+	}
+	
+	public void CliqueSurfaceDeTravail()
+	{
+		RaycastHit  hit;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		if (Physics.Raycast(ray, out hit)) 
+		{
+			Debug.Log(hit.transform.name);
+        }
+		if (ActiveButton == ButtonDisque1) {}
 	}
 }
