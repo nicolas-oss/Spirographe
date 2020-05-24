175a1b1a6477e7c6b31cd824119073e6864920b6
nicolas-oss <59309078+nicolas-oss@users.noreply.github.com>
2020-05-24 16:34:29 +0200

    ajout fonction screenshot

---
+++ Unity/Assets/Scripts/screen.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screen : MonoBehaviour
+{
+	public int resWidth = 1920; 
+	public int resHeight = 1080;
+	
+	public void ScreenShot()
+    {
+        ScreenCapture.CaptureScreenshot("SpiroScreenShot"+System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss")+".png",1);
+    }
+}
