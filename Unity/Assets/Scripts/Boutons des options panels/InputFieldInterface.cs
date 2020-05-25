1d56102c9bb5f08e57a92cd3c40f79c7f930b698
nicolas-oss <59309078+nicolas-oss@users.noreply.github.com>
2020-05-25 13:04:01 +0200

    InputField Draggable

--- Unity/Assets/Scripts/Boutons des options panels/InputFieldInterface.cs
+++ Unity/Assets/Scripts/Boutons des options panels/InputFieldInterface.cs
@@ -7,47 +7,65 @@ using UnityEngine.Events;
 public class InputFieldInterface : MonoBehaviour
 {
     public GameObject Interface;
-	
-	// Start is called before the first frame update
-    /*void Start()
-    {
-		Button btn = GetComponent<Button>();
-		btn.onClick.AddListener(TaskOnClick); 
-    }*/
+	float ValeurInitiale,ValeurSortie;
+	//public GameObject InputFieldToRefresh;
+	SpiroFormule SelectedLine;
+	public float FacteurDiv = 100.0f;
+	public string InputID;
+	InputField MainInputField;
 	
 	void Start()
 	{
 		SelectedLine = Interface.GetComponent<Interface>().SelectedLine;
+		MainInputField=gameObject.GetComponent<InputField>();
+		MainInputField.onEndEdit.AddListener(delegate {SetActiveEvent(); });
 	}
 	
-	/*void TaskOnClick()
+	void onPointerClick()
 	{
-		Interface.GetComponent<Interface>().SelectButton(gameObject);
 		SetActiveEvent();
-	}*/
+	}
 	
+	void onMouseDOwn()
+	{
+		BeginAjusteWithDrag();
+	}
+	
+	void TaskOnClick()
+	{
+		//Interface.GetComponent<Interface>().SelectButton(gameObject);
+		SetActiveEvent();
+	}
+
 	public void SetActiveEvent()
 	{
-		Interface.GetComponent<Interface>().MainDragEvent.RemoveAllListeners();;
+		SelectedLine = Interface.GetComponent<Interface>().SelectedLine;
+		Interface.GetComponent<Interface>().MainDragEvent.RemoveAllListeners();
 		Interface.GetComponent<Interface>().FirstDragEvent.RemoveAllListeners();
-		Interface.GetComponent<Interface>().FirstDragEvent.AddListener(delegate
-        {
-            BeginAjusteWithDrag(string InputID);
-        });
+		Interface.GetComponent<Interface>().FirstDragEvent.AddListener(BeginAjusteWithDrag);
 		Interface.GetComponent<Interface>().MainDragEvent.AddListener(AjusteWithDrag);
+		switch (InputID)
+		{
+			case "A1": ValeurSortie = SelectedLine.GetComponent<SpiroFormule>().A1; break;
+			case "V1": ValeurSortie = SelectedLine.V1; break;
+			case "P1": ValeurSortie = SelectedLine.P1; break;
+		}
 	}
 	
-	public void BeginAjusteWithDrag(string InputID)
+	public void BeginAjusteWithDrag()
 	{
-		ValeurInitiale = SelectedLine.A1;
+		ValeurInitiale = ValeurSortie; 
+	}
+	
+	public void AjusteWithDrag()
+	{
+		ValeurSortie = ValeurInitiale + Interface.GetComponent<Interface>().DeltaMousePos.x/FacteurDiv;
+		GetComponent<InputField>().text = ValeurSortie.ToString();
 		switch (InputID)
-      {
-         case "A1": ValeurInitiale = SelectedLine.A1;
-            break;
-         case "V1": ValeurInitiale = SelectedLine.V1;
-            break;
-         case "P1": ValeurInitiale = SelectedLine.P1;
-            break;
-      }
+		{
+			case "A1": SelectedLine.A1 = ValeurSortie; break;
+			case "V1": SelectedLine.V1 = ValeurSortie; break;
+			case "P1": SelectedLine.P1 = ValeurSortie; break;
+		}
 	}
 }
