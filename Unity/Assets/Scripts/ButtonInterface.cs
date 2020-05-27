489943195819ac78360b32d67bf6804d32fe972e
nicolas-oss <59309078+nicolas-oss@users.noreply.github.com>
2020-05-27 14:21:34 +0200

    butoon interface sans ref à interface

--- Unity/Assets/Scripts/ButtonInterface.cs
+++ Unity/Assets/Scripts/ButtonInterface.cs
@@ -6,8 +6,9 @@ using UnityEngine.Events;
 
 public class ButtonInterface : MonoBehaviour
 {
-    public GameObject Interface;
-	// Start is called before the first frame update
+    public GameObject OptionPanel;
+	public Color UnselectedColor,SelectedColor;
+
     void Start()
     {
 		Button btn = GetComponent<Button>();
@@ -16,6 +17,64 @@ public class ButtonInterface : MonoBehaviour
 	
 	void TaskOnClick()
 	{
-		Interface.GetComponent<Interface>().SelectButton(gameObject);
+		SelectButton(gameObject);
+	}
+	
+	public void SelectButton(GameObject PressedButton)
+	{
+		DeselectAllButton();
+		HideAllOptionPanels();
+		SetColorActive();
+		//ActiveButton = PressedButton;
+		ShowCurrentOptionPanel();
+		SetActiveEvent();
+	}
+	
+	public void DeselectAllButton()
+	{
+		GameObject[] AllButtons;
+		AllButtons = GameObject.FindGameObjectsWithTag("Button");
+		foreach(GameObject InspectedButton in AllButtons)
+		{
+			var InspectedMyButton=InspectedButton.GetComponent<Button>();
+			var InspectedColors = InspectedMyButton.colors;
+			InspectedColors.normalColor = UnselectedColor;
+			InspectedMyButton.colors = InspectedColors;
+		}
+	}
+	
+	public void HideAllOptionPanels()
+	{
+		GameObject[] AllOptionPanels;
+		AllOptionPanels = GameObject.FindGameObjectsWithTag("PanelOption");
+		foreach(GameObject InspectedPanel in AllOptionPanels)
+		{
+			InspectedPanel.active=false;
+		}	
+	}
+	
+	public void SetColorActive()
+	{
+		var MyButton=GetComponent<Button>();
+		var colors = MyButton.colors;
+        colors.normalColor = SelectedColor;
+        MyButton.colors = colors;
+	}
+	
+	public void ShowCurrentOptionPanel()
+	{
+		OptionPanel.active=true;
+	}
+	
+	public void SetActiveEvent()
+	{
+		/*MainDragEvent.RemoveAllListeners();
+		FirstDragEvent.RemoveAllListeners();
+		if (ActiveButton==ButtonEchelle) {FirstDragEvent.AddListener(BeginAjusteEchelleWithDrag); MainDragEvent.AddListener(AjusteEchelleWithDrag);}
+		if (ActiveButton==ButtonRotation) {FirstDragEvent.AddListener(BeginAjusteRotationWithDrag); MainDragEvent.AddListener(AjusteRotationWithDrag);}
+		if (ActiveButton==ButtonDisque1) {FirstDragEvent.AddListener(BeginAjusteDisque1WithDrag); MainDragEvent.AddListener(AjusteDisque1WithDrag);}
+		if (ActiveButton==ButtonDisque2) {FirstDragEvent.AddListener(BeginAjusteDisque2WithDrag); MainDragEvent.AddListener(AjusteDisque2WithDrag);}
+		if (ActiveButton==ButtonDisque3) {FirstDragEvent.AddListener(BeginAjusteDisque3WithDrag); MainDragEvent.AddListener(AjusteDisque3WithDrag);}
+		if (ActiveButton==ButtonCrayon) {FirstDragEvent.AddListener(BeginAjusteCXYWithDrag); MainDragEvent.AddListener(AjusteCXYWithDrag);}*/
 	}
 }
